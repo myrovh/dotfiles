72fd18b115fec6f06fddd8f1654af50067182fa6
myrovh <myrovh@yandex.com>
2017-03-25 16:22:51 +1100

    i3 with polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,12 @@
-#! /usr/bin/env sh
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
 killall -q polybar
+
+# Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-polybar bar1 &
-echo "Bars launched"
+
+# Launch bar1 and bar2
+polybar default &
+
+echo "Bars launched..."
