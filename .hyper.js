dc738131c498b4e0998c70b81e6bf00271db4fb8
myrovh <myrovh@yandex.com>
2017-05-05 22:50:04 +1000

    switched to materia

--- .hyper.js
+++ .hyper.js
@@ -97,6 +97,6 @@ module.exports = {
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: [
-	  'hyperterm-base16-isotope-dark'
+	  'hyperterm-base16-materia'
   ]
 };
