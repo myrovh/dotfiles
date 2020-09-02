1152e533c4866ee4aefde0780bc53f0b8c816087
myrovh <myrovh@protonmail.com>
2020-09-02 18:13:54 +1000

    update doom config

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -46,7 +46,7 @@
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces          ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere)  ; come to the dark side, we have cookies
@@ -138,7 +138,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +dragndrop +journal)         ; organize your plain life in plain text
+       (org +roam +dragndrop +journal +pretty)         ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
