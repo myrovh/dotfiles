d8053977db8c997b857a68354b684fba17eaa559
myrovh <myrovh@protonmail.com>
2020-10-17 23:28:33 +1100

    adjust doom emacs modules

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -19,13 +19,13 @@
        ;;japanese
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +prescient +childframe +icons)               ; a search engine for love and life
+       (company +childframe)                      ; the ultimate code completion backend
+       ;;helm                                     ; the *other* search engine for love and life
+       ;;ido                                      ; the other *other* search engine...
+       (ivy +fuzzy +prescient +childframe +icons) ; a search engine for love and life
 
        :ui
-       deft              ; notational velocity for Emacs
+       deft                ; notational velocity for Emacs
        doom                ; what makes DOOM look the way it does
        doom-dashboard      ; a nifty splash screen for Emacs
        doom-quit           ; DOOM quit-message prompts when you quit Emacs
@@ -40,7 +40,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;pretty-code       ; ligatures or substitute text with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs            ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
@@ -73,12 +73,12 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       vterm               ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
-       (spell +hunspell)   ; tasing you for misspelling mispelling
-       grammar             ; tasing grammar mistake every you make
+       syntax                        ; tasing you for every semicolon you forget
+       (spell +flyspell +hunspell)   ; tasing you for misspelling mispelling
+       grammar                       ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
@@ -138,7 +138,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +dragndrop +journal +pretty)         ; organize your plain life in plain text
+       (org +roam +dragndrop +journal )         ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
