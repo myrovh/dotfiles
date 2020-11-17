0fd240d951a9d90605a7921862b2c2e3c1fea5a0
myrovh <myrovh@protonmail.com>
2020-11-18 09:58:16 +1100

    remove slow spell checking

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -76,9 +76,9 @@
        vterm               ; the best terminal emulation in Emacs
 
        :checkers
-       syntax                        ; tasing you for every semicolon you forget
-       (spell +flyspell +hunspell)   ; tasing you for misspelling mispelling
-       grammar                       ; tasing grammar mistake every you make
+       ;syntax                        ; tasing you for every semicolon you forget
+       spell             ; tasing you for misspelling mispelling
+       ;grammar                       ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
